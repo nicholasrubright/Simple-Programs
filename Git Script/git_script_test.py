0ff4aeb89d8064412955e2589c0da19fc4639066
nicholasrubright <42792408+nicholasrubright@users.noreply.github.com>
2019-06-07 15:59:59 -0400

    updated program files

--- Git Script/git_script_test.py
+++ Git Script/git_script_test.py
@@ -20,18 +20,13 @@ def main():
 
     folder = StringVar()
     folderCombobox = ttk.Combobox(root, textvariable = folder)
-    folderCombobox.grid(row = 0, column = 0, stick = 'nsew', padx=20, pady=5)
+    folderCombobox.grid(row = 0, column = 1, stick = 'nsew', pady = 10, padx = 50)
     folderCombobox.config(values = projectFolders)
 
     copybutton = ttk.Button(root, text = "Copy file")
-    copybutton.grid(row = 0, column = 4, padx=10)   
+    copybutton.grid(row = 0, column = 4, stick = 'nsew', padx = 10, pady = 10)   
 
     selectedFolder = str(folder.get())
-
-    readmeValue = IntVar()
-    Checkbutton(root, text="Create README", variable = readmeValue).grid(row = 1, column = 0, stick = 'nsew')
-    headerValue = IntVar()
-    Checkbutton(root, text="Include Header", variable = headerValue).grid(row = 2, column = 0, stick ='nsew')
     
     def copy():
         selectedFolder = str(folder.get())
@@ -43,18 +38,7 @@ def main():
                         file = selectedFolder + ".cpp"
                         filepath = os.path.realpath('C:\\Users\\Nicholas\\source\\repos\\' + selectedFolder + '\\' + selectedFolder + '\\' + file)
                         shutil.copy(filepath, gitpath)
-
-                        #os.mkdir(selectedFolder)
-                        #shutil.move(selectedFolder, gitpath)
-                        #shutil.move(filepath, selectedFolder)
-                        
-                if headerValue.get() == 1 and filename.endswith(".h") and filename != 'pch.h':
-                    headpath = os.path.realpath('C:\\Users\\Nicholas\\source\\repos\\' +  selectedFolder + '\\' + selectedFolder + '\\' + filename)
-                    shutil.copy(headpath, gitpath)
-        if readmeValue.get() == 1:
-            findLoc = open(gitpath + "\\" + "README.md", 'w')
-            findLoc.close()
-        
+    
     copybutton.config(command = copy)
 
 if __name__ == "__main__":
